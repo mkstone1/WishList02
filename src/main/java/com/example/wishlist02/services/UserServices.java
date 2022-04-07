0939d472a1ed4ae90e5c7b8ca557eb4b124abe6b
mms <mark425e@stud.kea.dk>
2022-04-07 12:10:28 +0200

    rettet så username ikke er case sensistive

--- src/main/java/com/example/wishlist02/services/UserServices.java
+++ src/main/java/com/example/wishlist02/services/UserServices.java
@@ -3,12 +3,13 @@ package com.example.wishlist02.services;
 import com.example.wishlist02.Model.User;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class UserServices {
 
     public String checkUser(String username, String password, ArrayList<User> allUsers){
         for(User user : allUsers){
-            if(username.equals(user.getUsername()) && password.equals(user.getPassword())){
+            if(username.toLowerCase().equals(user.getUsername().toLowerCase()) && password.equals(user.getPassword())){
                 return user.getUserID();
             }
         }
